980ac3da249fb38461f885b8ca1b1ed5af01e398
Sergioliva-1 <yimioliva.sos@email.com>
2020-01-10 17:08:02 -0300

    agregue cambio

--- 03 Sentando las bases/01 querySelector/scripts/main.js
+++ 03 Sentando las bases/01 querySelector/scripts/main.js
@@ -1,1 +1,11 @@
 console.info('Cuando veas éste mensaje en consola, ya habrás terminado ésta parte del ejercicio. Borra ésta línea cuando ya la hayas visto alguna vez en la consola al acceder a ésta página.')
+
+const comentarios = document.querySelectorAll(".comment")
+for(let i = 0; i< comentarios.length; i++){
+    comentarios[1].style.background = "green";
+}
+
+const linea = document.querySelectorAll ('hr')
+for (let i = 0;i< linea.length;i++){
+    linea[i].style.background = "red";
+}
